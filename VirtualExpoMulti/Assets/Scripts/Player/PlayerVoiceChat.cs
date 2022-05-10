8759706eb110351b7577db505d877a3636fb736f
Bezaleel.Ariessanta <bezaleel.ariessanta@primecash.co.id>
2022-05-10 14:22:05 +0700

    Voice Chat Core (done)
    - adding Speaker to all character (inside of it had : Photon Speaker and audio sources)
    - player voice chat script for enable / disable ui image (voice indicator for player)
    - adding voice view to player
    - adding prefab voiceManager to Scene Main Area and Main Area 01 (inside of it had photon recorder and photon voice network)
    - adding push-to-talk methods in voicechat script
    
    bugs :
    - error at Photon Voice SDK when build to IOS / Mac OS

--- VirtualExpoMulti/Assets/Scripts/Player/PlayerVoiceChat.cs
+++ VirtualExpoMulti/Assets/Scripts/Player/PlayerVoiceChat.cs
@@ -9,60 +9,109 @@ using Photon.Voice.Unity;
 using System;
 
 
-public class PlayerVoiceChat : MonoBehaviour
+namespace VirtualExpo.MainArea.PlayerUIVoice
 {
 
-    [SerializeField]
-    private Image micImage;
-
-    [SerializeField]
-    private Image speakerImage;
-
-    [SerializeField]
-    private Image micDisableImage;
-
-    [SerializeField]
-    private TMP_Text bufferLagText;
-
-    [SerializeField]
-    private PhotonVoiceView photonVoiceView;
-
-    private void Awake()
+    public class PlayerVoiceChat : MonoBehaviour
     {
 
-        photonVoiceView = this.GetComponent<PhotonVoiceView>();
-        OnStartMicEvent();
+        [SerializeField]
+        private Image micImage;
 
-    }
+        [SerializeField]
+        private Image speakerImage;
 
-    private void Update()
-    {
+        [SerializeField]
+        private Image micDisableImage;
 
-        OnUpdateMicEvent();
+        [SerializeField]
+        private TMP_Text bufferLagText;
 
-    }
+        [SerializeField]
+        private PhotonVoiceView photonVoiceView;
 
-    private void OnStartMicEvent()
-    {
+        private PhotonView PV;
 
-        this.micImage.enabled = false;
-        this.speakerImage.enabled = false;
-        this.micDisableImage.enabled = true;
+        [SerializeField]
+        private Recorder recorder;
 
-    }
-
-    private void OnUpdateMicEvent()
-    {
-
-        this.micImage.enabled = this.photonVoiceView.IsRecording;
-        this.speakerImage.enabled = this.photonVoiceView.IsSpeaking;
-
-        this.bufferLagText.enabled = this.photonVoiceView.IsSpeaking;
-        if (this.bufferLagText.enabled)
+        private void Awake()
         {
-            this.bufferLagText.text = string.Format("{0}", this.photonVoiceView.SpeakerInUse.Lag);
+
+            photonVoiceView = this.GetComponent<PhotonVoiceView>();
+            recorder = GameObject.FindGameObjectWithTag("VoiceChatManager").GetComponent<Recorder>();
+            PV = this.GetComponent<PhotonView>();
+            OnStartMicEvent();
+
+        }
+
+        private void Update()
+        {
+
+            PushToTalk();
+            //OnUpdateMicEvent();
+
+        }
+
+        private void PushToTalk()
+        {
+
+            if (PV.IsMine)
+            {
+
+                //if user pressed "V" on Keyboard
+                //then enable the TransmitEnabled
+                if (Input.GetKey(KeyCode.V))
+                {
+
+                    recorder.TransmitEnabled = true;
+                    OnUpdateMicEvent();
+                    this.micDisableImage.enabled = !recorder.TransmitEnabled;
+
+                }
+                else
+                {
+
+                    recorder.TransmitEnabled = false;
+                    this.micDisableImage.enabled = !recorder.TransmitEnabled;
+
+                }
+
+            }
+            else
+            {
+
+                this.micDisableImage.enabled = false;
+
+            }
+
+        }
+
+        private void OnStartMicEvent()
+        {
+
+            this.micImage.enabled = false;
+            this.speakerImage.enabled = false;
+            this.bufferLagText.enabled = false;
+            this.micDisableImage.enabled = false;
+
+        }
+
+        private void OnUpdateMicEvent()
+        {
+
+            this.micImage.enabled = this.photonVoiceView.IsRecording;
+            this.speakerImage.enabled = this.photonVoiceView.IsSpeaking;
+
+            /*this.bufferLagText.enabled = this.photonVoiceView.IsSpeaking;
+            if (this.bufferLagText.enabled)
+            {
+                this.bufferLagText.text = string.Format("{0}", this.photonVoiceView.SpeakerInUse.Lag);
+            }*/
+
         }
 
     }
+
 
 }
