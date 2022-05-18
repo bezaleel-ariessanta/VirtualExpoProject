4b30b0cf87dfbbbd8f78cdcd53cd51f577e80ef0
Bezaleel.Ariessanta <bezaleel.ariessanta@primecash.co.id>
2022-05-18 14:02:26 +0700

    Public Chat and PopUp Messages (Fixed)
    - bug fix @ PlayerChatMessage.cs that has been put @ Prefabs Player Character
      note : other person that chatting in public room can't be shown even popup message have no problem to show other person message. (Fixed)
    - added ChatListManager.cs (for dumping messages) so can be shown everytime someone setting chat
    
    - now for all main scene.

--- VirtualExpoMulti/Assets/Scripts/Constant/PlayerControllerConstant.cs
+++ VirtualExpoMulti/Assets/Scripts/Constant/PlayerControllerConstant.cs
@@ -7,11 +7,11 @@ public class PlayerControllerConstant
     public const KeyCode ACTIVE_MIC_KEY = KeyCode.V;
     public const KeyCode ACTIVE_CHAT_MESSAGE_KEY = KeyCode.Tab;
 
-#if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+#if UNITY_EDITOR_OSX || PLATFORM_STANDALONE_OSX || UNITY_STANDALONE_OSX
 
     public const KeyCode SEND_THE_MESSAGE_ENTER = KeyCode.Return;
 
-#elif UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+#elif UNITY_EDITOR_WIN || PLATFORM_STANDALONE || UNITY_STANDALONE
 
     public const KeyCode SEND_THE_MESSAGE_ENTER = KeyCode.KeypadEnter;
 
